4ee6929cbb218400bc51aff91cf7e6805543d599
Joseph-Kimuhu <joseph.kimuhu@student.moringaschool.com>
2025-12-31 11:01:05 +0300

    final commit

--- server/app.py
+++ server/app.py
@@ -4,7 +4,7 @@ from flask import Flask, jsonify, request, make_response
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
 
-from models import db, Plant
+from .models import db, Plant
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///plants.db'
@@ -44,8 +44,28 @@ api.add_resource(Plants, '/plants')
 class PlantByID(Resource):
 
     def get(self, id):
-        plant = Plant.query.filter_by(id=id).first().to_dict()
-        return make_response(jsonify(plant), 200)
+        plant = Plant.query.filter_by(id=id).first()
+        if not plant:
+            return make_response(jsonify({"error": "Plant not found"}), 404)
+        return make_response(plant.to_dict(), 200)
+
+    def patch(self, id):
+        plant = Plant.query.filter_by(id=id).first()
+        if not plant:
+            return make_response(jsonify({"error": "Plant not found"}), 404)
+        data = request.get_json()
+        for attr in data:
+            setattr(plant, attr, data[attr])
+        db.session.commit()
+        return make_response(plant.to_dict(), 200)
+
+    def delete(self, id):
+        plant = Plant.query.filter_by(id=id).first()
+        if not plant:
+            return make_response(jsonify({"error": "Plant not found"}), 404)
+        db.session.delete(plant)
+        db.session.commit()
+        return make_response('', 204)
 
 
 api.add_resource(PlantByID, '/plants/<int:id>')
